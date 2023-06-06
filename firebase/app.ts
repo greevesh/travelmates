b977de7f630f97a59a58d19a244509b93a25d503
greevesh <hgreeves@protonmail.com>
2023-06-07 01:21:35 +0800

    set up google sign in

---
+++ firebase/app.ts
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+import { collection, getDocs, getFirestore } from "firebase/firestore"; 
+import firebaseConfig from '@root/firebase/config';
+
+// Initialize Firebase
+// const db = getFirestore(firebaseApp);
+
+// const querySnapshot = await getDocs(collection(db, "dates"));
+// console.log(querySnapshot.docs.map(doc => doc.data()));
