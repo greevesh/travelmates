4b944e43584786d36eb7560efd5f6d6f52236b12
greevesh <hgreeves@protonmail.com>
2023-06-09 19:18:56 +0800

    removed sign out button from groups page

--- src/app/groups/page.tsx
+++ src/app/groups/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import SignOutBtn from "../components/Header/SignOutBtn";
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "@root/firebase/config";
 
@@ -10,7 +9,6 @@ export default function Groups() {
     return (
         <div>
         <h1>Groups</h1>
-        <SignOutBtn />
         </div>
     )
 }
