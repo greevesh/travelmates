cac88ffdfd9ac05da37f15f7ecf490983fa76241
greevesh <hgreeves@protonmail.com>
2023-06-12 05:08:40 +0800

    slight refactor on sign out btn component

--- src/app/components/Header/SignOutBtn.tsx
+++ src/app/components/Header/SignOutBtn.tsx
@@ -1,23 +1,23 @@
 import { signOut as logOut, getAuth } from "firebase/auth";
 import { AuthError } from "../../lib/exceptions";
-import { redirect } from "../../helpers";
 import Button from "react-bootstrap/Button";
 import { type Auth } from "firebase/auth";
 import firebaseConfig from "@root/firebase/config";
 import { initializeApp } from "firebase/app";
 import { useRouter } from "next/navigation";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context";
 
 initializeApp(firebaseConfig);
 
 const SignOutBtn = () => {
   const auth: Auth = getAuth();
-  const router = useRouter();
+  const router: AppRouterInstance = useRouter();
 
   const signOut = (): void => {
     try {
       logOut(auth).then(() => {
         router.push("/");
-        console.log("signed out");
+        console.log("signed out", auth.currentUser);
       });
     } catch (error) {
       throw new AuthError("There was an error signing out.");
