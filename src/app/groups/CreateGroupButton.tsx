60e921decc7ef3e13fe56770542353e443b34fc8
greevesh <hgreeves@protonmail.com>
2023-07-31 14:48:12 +0800

    create group button component created

---
+++ src/app/groups/CreateGroupButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { CreateGroupButtonProps } from "../types";
+
+const CreateGroupButton: React.FC<CreateGroupButtonProps> = ({
+  group,
+  handleSubmit,
+  emptyInput,
+}) => {
+  return (
+    <Button disabled={emptyInput} onClick={() => handleSubmit()}>
+      Create Group
+    </Button>
+  );
+};
+
+export default CreateGroupButton;
