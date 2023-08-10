f5655c2b8f9d6015e85be656cd571ea216099717
greevesh <hgreeves@protonmail.com>
2023-08-10 19:37:28 +0800

    root styles have been moved

--- src/app/group/page.tsx
+++ src/app/group/page.tsx
@@ -10,33 +10,9 @@ import rows from "./rows";
 import NextMonthButton from "./NextMonthButton";
 import generateCalendar from "./generateCalendar";
 import { slotColumnCommonFields } from "./columns";
+import { rootStyles } from "./rootStyles";
 
 initializeApp(firebaseConfig);
-
-const rootStyles = {
-  width: "100%",
-  "& .Cebu": {
-    backgroundColor: "rgba(157, 255, 118, 0.49)",
-  },
-  "& .Moalboal": {
-    backgroundColor: "rgba(255, 255, 10, 0.49)",
-  },
-  "& .Hanoi": {
-    backgroundColor: "rgba(150, 150, 150, 0.49)",
-  },
-  "& .Taipei": {
-    backgroundColor: "rgba(255, 150, 150, 0.49)",
-  },
-  "& .Brisbane": {
-    backgroundColor: "rgba(10, 150, 255, 0.49)",
-  },
-  "& .Tokyo": {
-    backgroundColor: "rgba(224, 183, 60, 0.55)",
-  },
-  "& .Singapore": {
-    backgroundColor: "rgba(122, 153, 60, 0.49)",
-  },
-};
 
 export default function ColumnSpanningDerived() {
   const [selectedMonth, setselectedMonth] = useState(new Date().getMonth());
