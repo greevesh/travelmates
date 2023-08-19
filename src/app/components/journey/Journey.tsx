b6276832e62d698743f3c028765114ca8a4b1f94
greevesh <hgreeves@protonmail.com>
2023-08-20 00:11:20 +0800

    formatDate func has been moved

--- src/app/components/journey/Journey.tsx
+++ src/app/components/journey/Journey.tsx
@@ -194,20 +194,6 @@ const Journey: React.FC = () => {
     setEmptyInput(true);
   };
 
-  const formatDate = (timestamp: number | undefined): string => {
-    if (timestamp !== undefined) {
-      const date = new Date(timestamp * 1000);
-      const options: Intl.DateTimeFormatOptions = {
-        weekday: "short",
-        day: "numeric",
-        month: "short",
-        year: "numeric",
-      };
-      return date.toLocaleString("en-GB", options);
-    }
-    return "";
-  };
-
   return (
     <div>
       <Search
