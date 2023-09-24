47d029e26c5d75473827941c3008ff4914b22d22
greevesh <hgreeves@protonmail.com>
2023-09-24 17:56:47 +0700

    journey can now be rendered with correct date range

--- src/app/group/rows.ts
+++ src/app/group/rows.ts
@@ -6,8 +6,12 @@ import {
   type QuerySnapshot,
   type Query,
 } from "firebase/firestore";
-import { Row } from "../group/types";
 import { db } from "../../../firebase/app";
+import formatDate from "../create-journey/formatDate";
+import { months } from "./columns";
+import { Journey } from "../create-journey/types";
+import { Row } from "../group/types";
+
 import { currentUserID } from "../globals";
 
 async function fetchUserDisplayName() {
@@ -28,41 +32,100 @@ async function fetchUserDisplayName() {
 export const fetchCurrentUserJourneys = async () => {
   const locations: string[] = [];
   const q: Query<Document> = query(
-    collection(db, "journeys"),
-    where("userID", "==", currentUserID)
+    collection(db, "journeys")
+    // where("userID", "==", currentUserID)
   );
 
   const journeys: QuerySnapshot<unknown> = await getDocs(q);
-  console.log("current user journeys:", journeys);
+  const journeyDateRangeLengths = await fetchJourneyDateRangeLengths();
+  const journeyDateRanges = await fetchJourneyDateRanges();
+  const journeyStartDay: number = journeyDateRanges[0].start.getDate();
 
   if (journeys.size > 0) {
     journeys.forEach((doc) => {
       const journey = doc.data();
-      locations.push(journey.location);
+      for (let i = 0; i < journeyStartDay; i++) {
+        locations.push("");
+      }
+      for (let i = 0; i <= journeyDateRangeLengths[0]; i++) {
+        locations.push(journey.location);
+      }
     });
   }
-  console.log(locations);
+
   return locations;
+};
+
+export const fetchJourneyDateRanges = async (journey?: Journey) => {
+  const dateRanges: { start: Date; end: Date }[] = [];
+  const q: Query<Document> = query(collection(db, "journeys"));
+
+  const journeys: QuerySnapshot<unknown> = await getDocs(q);
+
+  if (journeys.size > 0) {
+    journeys.forEach((doc) => {
+      const journey = doc.data();
+      const { start, end } = journey.dateRange;
+      const startDate: Date = formatDate(start.seconds);
+      const endDate: Date = formatDate(end.seconds);
+      dateRanges.push({ start: startDate, end: endDate });
+    });
+  }
+
+  return dateRanges;
+};
+
+export const fetchJourneyDateRangeLengths = async () => {
+  const dateRangeLengths: number[] = [];
+  const q: Query<Document> = query(collection(db, "journeys"));
+
+  const journeys: QuerySnapshot<unknown> = await getDocs(q);
+
+  if (journeys.size > 0) {
+    journeys.forEach((doc) => {
+      const journey = doc.data();
+      const { start, end } = journey.dateRange;
+      const startDate: Date = formatDate(start.seconds);
+      const endDate: Date = formatDate(end.seconds);
+      const daysInBetween: number = endDate - startDate;
+      const formattedDays = Math.floor(daysInBetween / (24 * 60 * 60 * 1000));
+      dateRangeLengths.push(formattedDays);
+    });
+  }
+
+  return dateRangeLengths;
 };
 
 async function fetchRows() {
   const currentUserDisplayName: string = await fetchUserDisplayName();
   const currentUserJourneys: string[] = await fetchCurrentUserJourneys();
+  const currentDateRangeLengths: number[] =
+    await fetchJourneyDateRangeLengths();
+  console.log("Current date range lengths:", currentDateRangeLengths);
+  const journeyDateRanges = await fetchJourneyDateRanges();
+  const currentMonth: number = journeyDateRanges[0].start.getMonth();
+  const currentYear: number = journeyDateRanges[0].start.getFullYear();
+  console.log("Current year:", currentYear);
+  console.log("Current user journeys:", currentUserJourneys);
+  console.log(
+    "Journey start day number:",
+    journeyDateRanges[0].start.getDate()
+  );
 
   const rows: Row[] = [
     {
       id: 1,
       name: currentUserDisplayName,
-      month: "August",
-      year: 2023,
+      month: months[currentMonth],
+      year: currentYear,
       locations: currentUserJourneys,
     },
     {
       id: 2,
       name: "Myo",
-      month: "September",
+      month: "October",
       year: 2023,
-      locations: ["Taipei (TW)", "Taipei (TW)"],
+      locations: ["Taipei (TW)", "Taipei (TW)", "Taipei (TW)", "Taipei (TW)"],
     },
   ];
 
