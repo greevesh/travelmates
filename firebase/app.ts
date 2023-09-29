fd3c8ffcc73e495194db12cab402297ebe81a276
greevesh <hgreeves@protonmail.com>
2023-09-29 15:05:50 +0700

    refactored firestore init

--- firebase/app.ts
+++ firebase/app.ts
@@ -1,6 +1,8 @@
-import { getFirestore, Firestore, setDoc, doc } from "firebase/firestore";
+import { getFirestore, Firestore } from "firebase/firestore";
 import firebaseConfig from "./config";
-import { initializeApp, FirebaseApp } from "../node_modules/firebase/app";
+import { getApps } from "../node_modules/firebase/app";
+import { initializeApp, type FirebaseApp } from "../node_modules/firebase/app";
 
-export const firebaseApp: FirebaseApp = initializeApp(firebaseConfig);
+export const firebaseApp: FirebaseApp =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 export const db: Firestore = getFirestore(firebaseApp);
