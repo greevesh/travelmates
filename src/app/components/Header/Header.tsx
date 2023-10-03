d2a228dee69bd9cc6535807160ba4c981add86f8
greevesh <hgreeves@protonmail.com>
2023-10-03 12:13:08 +0700

    sign out button now only displays when user is signed in

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,12 +1,27 @@
-"use client";
+import React, { useState, useEffect } from "react";
+import {
+  type User,
+  onAuthStateChanged,
+} from "../../../../node_modules/firebase/auth";
+import { auth } from "../../../../firebase/auth";
 import SignOutBtn from "./SignOutButton";
 
 const Header: React.FC = () => {
+  const [user, setUser] = useState<User | null>(auth.currentUser);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <nav>
       <div>
         <h4>TravelM@tes</h4>
-        <SignOutBtn />
+        {user ? <SignOutBtn /> : null}
       </div>
     </nav>
   );
