b9714ef65b735ab4c10577ff9fc7ab25f51851ce
greevesh <hgreeves@protonmail.com>
2023-10-07 19:19:10 +0700

    trimmed down rows file

--- src/app/group/rows.ts
+++ src/app/group/rows.ts
@@ -4,14 +4,10 @@ import { Row } from "../group/types";
 import fetchCurrentUserDisplayName from "./fetchCurrentUserDisplayName";
 import fetchCurrentUserJourneys from "./fetchCurrentUserJourneys";
 import fetchJourneyDateRanges from "./fetchJourneyDateRanges";
-import fetchJourneyDateRangeLengths from "./fetchJourneyDateRangeLengths";
 
 const fetchRows = async () => {
   const currentUserDisplayName: string = await fetchCurrentUserDisplayName();
   const currentUserJourneys: string[] = await fetchCurrentUserJourneys();
-  const currentDateRangeLengths: number[] =
-    await fetchJourneyDateRangeLengths();
-  console.log("Current date range lengths:", currentDateRangeLengths);
   const journeyDateRanges = await fetchJourneyDateRanges();
   const currentMonth: number = journeyDateRanges[0].start.getMonth();
   const currentYear: number = journeyDateRanges[0].start.getFullYear();
@@ -23,13 +19,6 @@ const fetchRows = async () => {
       month: months[currentMonth],
       year: currentYear,
       locations: currentUserJourneys,
-    },
-    {
-      id: 2,
-      name: "Myo",
-      month: "October",
-      year: 2023,
-      locations: ["Taipei (TW)", "Taipei (TW)", "Taipei (TW)", "Taipei (TW)"],
     },
   ];
 
