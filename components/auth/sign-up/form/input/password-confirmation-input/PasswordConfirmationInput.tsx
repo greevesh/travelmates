e9bcb0be035e6a8b3408b53e7909086af159a71b
greevesh <hgreeves@protonmail.com>
2024-04-04 11:33:25 +0700

    added password and confirm password inputs

---
+++ components/auth/sign-up/form/input/password-confirmation-input/PasswordConfirmationInput.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import BaseInput from '../../../../../base/input/BaseInput'
+
+export default function PasswordConfirmationInput() {
+	return (
+		<BaseInput placeholder='Confirm password' secureText={true} />
+	)
+}
