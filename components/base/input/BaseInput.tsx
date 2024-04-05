38c9be97d110e5ec4f83ce2bd45a38f0b1cc4703
greevesh <hgreeves@protonmail.com>
2024-04-05 16:34:59 +0700

    removed stylesheet from base input

--- components/base/input/BaseInput.tsx
+++ components/base/input/BaseInput.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { StyleSheet } from 'react-native'
 import { TextInput } from 'react-native-paper'
 
 interface IBaseInputProps {
@@ -11,13 +10,7 @@ interface IBaseInputProps {
 export default function BaseInput({placeholder, secureText, testId}: IBaseInputProps) {
 	const [text, setText] = useState('')
 
-	const styles = StyleSheet.create({
-		input: {
-			backgroundColor: '#fff'
-		}
-	})
-
 	return (
-		<TextInput placeholder={placeholder} secureTextEntry={secureText} style={styles.input} value={text} onChangeText={setText} testID={testId} />
+		<TextInput placeholder={placeholder} secureTextEntry={secureText} style={{backgroundColor: '#fff'}} value={text} onChangeText={setText} testID={testId} />
 	)
 }
