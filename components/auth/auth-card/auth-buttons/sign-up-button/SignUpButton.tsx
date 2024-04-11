e2d54de2443919c8c92e71d44d0d91f09cc018a9
greevesh <hgreeves@protonmail.com>
2024-04-11 15:45:48 +0700

    sign up button moved

---
+++ components/auth/auth-card/auth-buttons/sign-up-button/SignUpButton.tsx
@@ -0,0 +1,5 @@
+import BaseButton from '../../../../base/button/BaseButton'
+
+export default function SignUpButton() {
+	return <BaseButton testId="sign-up-button">Sign up</BaseButton>
+}
--- components/auth/auth-card/sign-up-button/SignUpButton.tsx
+++
@@ -1,7 +0,0 @@
-import BaseButton from '../../../base/button/BaseButton'
-
-export default function SignUpButton() {
-	return (
-		<BaseButton testId='sign-up-button'>Sign up</BaseButton>
-	)
-}
