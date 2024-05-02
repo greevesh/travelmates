82ac63b94e6e5820967ea0f8672eaafa978495db
greevesh <hgreeves@protonmail.com>
2024-05-02 10:14:18 +0700

    validation msg shortened

--- components/auth/sign-up/form/schema.ts
+++ components/auth/sign-up/form/schema.ts
@@ -4,7 +4,7 @@ export const schema = z
 	.object({
 		email: z.string().email(),
 		password: z.string().min(8, 'Passwords must contain at least 8 characters')
-			.regex(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/, 'Passwords must contain a letter, number, and one special character'),
+			.regex(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/, 'Passwords need a letter, number, and symbol'),
 		passwordConfirmation: z.string(),
 	})
 	.refine((data) => data.password === data.passwordConfirmation, {
