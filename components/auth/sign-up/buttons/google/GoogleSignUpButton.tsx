b257eac6b3d326b3fc2eb0ccb288d1ae5772efa2
greevesh <hgreeves@protonmail.com>
2024-05-04 13:56:28 +0700

    temp fix for missing prop

--- components/auth/sign-up/buttons/google/GoogleSignUpButton.tsx
+++ components/auth/sign-up/buttons/google/GoogleSignUpButton.tsx
@@ -3,6 +3,7 @@ import BaseButton from '../../../../base/Button'
 export default function GoogleSignUpButton() {
 	return (
 		<BaseButton
+			onPress={() => console.log('pressed')}
 			text="Sign up with Google"
 			icon={{ source: 'google', size: 25 }}
 			bgColor="#4285F4"
