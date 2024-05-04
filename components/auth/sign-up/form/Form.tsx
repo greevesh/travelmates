e2cd907bac19674045eebfa9d3ad5cb657fb2e37
greevesh <hgreeves@protonmail.com>
2024-05-04 13:55:22 +0700

    dummy submission handling and validation added to sign up form

--- components/auth/sign-up/form/Form.tsx
+++ components/auth/sign-up/form/Form.tsx
@@ -1,16 +1,40 @@
 import { View } from 'react-native'
-import InputRows from '../input-rows/InputRows'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { schema, FormFields } from './schema'
 import AlreadyHaveAccount from '../already-have-account/AlreadyHaveAccount'
 import SignUpButton from '../buttons/traditional/SignUpButton'
-import GoogleSignUpButton from '../buttons/google/GoogleSignUpButton'
+import Email from '../inputs/email/Email'
+import Password from '../inputs/password/Password'
+import PasswordConfirmation from '../inputs/password/password-confirmation/PasswordConfirmation'
+import Error from '../../errors/Error'
 
 export default function SignUpForm() {
+	const {
+		control,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormFields>({
+		defaultValues: {
+			email: '',
+			password: '',
+			passwordConfirmation: '',
+		},
+		resolver: zodResolver(schema),
+	})
+
+	const onSubmit = (data: FormFields) => console.log(data)
+
 	return (
-		<View testID='sign-up-form'>
-			<InputRows />
+		<View>
+			<Email control={control} />
+			<Error msg={errors.email?.message} />
+			<Password control={control} />
+			<Error msg={errors.password?.message} />
+			<PasswordConfirmation control={control} />
+			<Error msg={errors.passwordConfirmation?.message} />
 			<AlreadyHaveAccount />
-			<SignUpButton />
-			<GoogleSignUpButton />
+			<SignUpButton onPress={handleSubmit(onSubmit)} />
 		</View>
 	)
 }
