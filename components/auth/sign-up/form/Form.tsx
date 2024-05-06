956cdea8c70774d5fa956c1f49d565e296e1747c
greevesh <hgreeves@protonmail.com>
2024-05-06 16:16:28 +0700

    fixed linting querks

--- components/auth/sign-up/form/Form.tsx
+++ components/auth/sign-up/form/Form.tsx
@@ -1,40 +1,40 @@
-import { View } from 'react-native';
-import { useForm } from 'react-hook-form';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { schema, FormFields } from './schema';
-import AlreadyHaveAccount from '../AlreadyHaveAccount';
-import SignUpButton from '../buttons/SignUpButton';
-import Email from '../inputs/Email';
-import Password from '../inputs/password/Password';
-import PasswordConfirmation from '../inputs/password/PasswordConfirmation';
-import Error from '../../Error';
+import { View } from 'react-native'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { schema, FormFields } from './schema'
+import AlreadyHaveAccount from '../AlreadyHaveAccount'
+import SignUpButton from '../buttons/SignUpButton'
+import Email from '../inputs/Email'
+import Password from '../inputs/password/Password'
+import PasswordConfirmation from '../inputs/password/PasswordConfirmation'
+import Error from '../../Error'
 
 export default function SignUpForm() {
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormFields>({
-    defaultValues: {
-      email: '',
-      password: '',
-      passwordConfirmation: '',
-    },
-    resolver: zodResolver(schema),
-  });
+	const {
+		control,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormFields>({
+		defaultValues: {
+			email: '',
+			password: '',
+			passwordConfirmation: '',
+		},
+		resolver: zodResolver(schema),
+	})
 
-  const onSubmit = (data: FormFields) => console.log(data);
+	const onSubmit = (data: FormFields) => console.log(data)
 
-  return (
-    <View>
-      <Email control={control} />
-      <Error msg={errors.email?.message} />
-      <Password control={control} />
-      <Error msg={errors.password?.message} />
-      <PasswordConfirmation control={control} />
-      <Error msg={errors.passwordConfirmation?.message} />
-      <AlreadyHaveAccount />
-      <SignUpButton onPress={handleSubmit(onSubmit)} />
-    </View>
-  );
+	return (
+		<View>
+			<Email control={control} />
+			<Error msg={errors.email?.message} />
+			<Password control={control} />
+			<Error msg={errors.password?.message} />
+			<PasswordConfirmation control={control} />
+			<Error msg={errors.passwordConfirmation?.message} />
+			<AlreadyHaveAccount />
+			<SignUpButton onPress={handleSubmit(onSubmit)} />
+		</View>
+	)
 }
