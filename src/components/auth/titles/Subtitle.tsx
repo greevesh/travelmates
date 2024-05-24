10667fc436d70307558e9affc1c6104c3167f13f
greevesh <hgreeves@protonmail.com>
2024-05-24 13:21:44 +0700

    moved components

--- src/components/auth/titles/Subtitle.tsx
+++ src/components/auth/titles/Subtitle.tsx
@@ -1,12 +1,16 @@
 import { StyleSheet } from 'react-native'
 import { useNavigationState } from '@react-navigation/native'
 
-import BaseTitle from '../../../base/Title'
+import BaseTitle from '../../base/Title'
 
 export default function Subtitle() {
-	const screen = useNavigationState(state => state.routes[state.index].name)
+	const screen = useNavigationState((state) => state.routes[state.index].name)
 
-	return <BaseTitle style={styles.title}>{screen === 'Sign Up' ? 'Become a Travel Mate today' : 'Welcome back'}</BaseTitle>
+	return (
+		<BaseTitle style={styles.title}>
+			{screen === 'Sign Up' ? 'Become a Travel Mate today' : 'Welcome back'}
+		</BaseTitle>
+	)
 }
 
 const styles = StyleSheet.create({
--- src/components/auth/titles/Title.tsx
+++ src/components/auth/titles/Title.tsx
@@ -1,18 +1,14 @@
 import { StyleSheet } from 'react-native'
 import { useNavigationState } from '@react-navigation/native'
 
-import BaseTitle from '../../../base/Title'
+import BaseTitle from '../../base/Title'
 
 export default function Title() {
-	const screen = useNavigationState(state => state.routes[state.index].name)
+	const screen = useNavigationState((state) => state.routes[state.index].name)
 
 	console.log(screen)
 
-	return (
-		<BaseTitle style={styles.title}>
-			{screen === 'Sign Up' ? 'Sign up' : 'Sign in'} 
-		</BaseTitle>
-	)
+	return <BaseTitle style={styles.title}>{screen === 'Sign Up' ? 'Sign up' : 'Sign in'}</BaseTitle>
 }
 
 const styles = StyleSheet.create({
