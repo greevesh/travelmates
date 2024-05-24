260a70b0c853b703476332acfb9cdef3711f5d52
greevesh <hgreeves@protonmail.com>
2024-05-24 13:29:40 +0700

    updated imports

--- src/components/auth/Card.tsx
+++ src/components/auth/Card.tsx
@@ -1,12 +1,12 @@
 import { Card } from 'react-native-paper'
 
 import Image from './Image'
-import Title from './sign-up/titles/Title'
-import Subtitle from './sign-up/titles/Subtitle'
+import Title from './titles/Title'
+import Subtitle from './titles/Subtitle'
 import SignUpForm from './sign-up/form/Form'
 import GoogleSignUpButton from './sign-up/buttons/GoogleSignUpButton'
 
-export default function AuthCard() {
+export default function SignUpCard() {
 	return (
 		<Card style={{ width: '90%', backgroundColor: '#fff' }} mode="contained">
 			<Image />
