0904bdb39f1b57b273fb73a55dab3878ed6f290d
greevesh <hgreeves@protonmail.com>
2024-07-26 10:34:36 +0800

    implemented sign in func

--- src/components/auth/sign-in/Form.tsx
+++ src/components/auth/sign-in/Form.tsx
@@ -1,28 +1,56 @@
-import { View } from 'react-native'
+import { Alert, View } from 'react-native'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
+import axios, { AxiosError } from 'axios'
+import * as SecureStore from 'expo-secure-store'
 
-import SignInButton from '../sign-in/buttons/SignInButton'
 import Username from '../inputs/Username'
 import Password from '../inputs/password/Password'
 import Error from '../Error'
 import AuthScreenLink from '../AuthScreenLink'
-import { FormFields, schema } from '../sign-up/form/schema'
+import { SignInFormFields, signInSchema } from '../sign-up/form/schema'
+import { signInEndpoint } from '../../../consts/api'
+import SignInButton from './buttons/SignInButton'
 
 export default function SignInForm() {
 	const {
 		control,
 		handleSubmit,
 		formState: { errors },
-	} = useForm<FormFields>({
+	} = useForm<SignInFormFields>({
 		defaultValues: {
 			username: '',
 			password: '',
 		},
-		resolver: zodResolver(schema),
+		resolver: zodResolver(signInSchema),
 	})
 
-	const onSubmit = (data: FormFields) => console.log(data)
+	const onSubmit = async (data: SignInFormFields) => {
+		try {
+			const { username, password } = data
+			const res = await axios.post(signInEndpoint, { username, password })
+			const { accessToken, refreshToken } = res.data
+
+			await SecureStore.setItemAsync('accessToken', accessToken)
+			await SecureStore.setItemAsync('refreshToken', refreshToken)
+
+			Alert.alert('User successfully signed in!')
+		} catch (err) {
+			if (err instanceof AxiosError) {
+				if (err.response) {
+					if (err.response.status === 404) {
+						Alert.alert('User does not exist')
+					}
+					if (err.response.status === 401) {
+						Alert.alert('Incorrect password')
+					}
+				}
+			}
+			else {
+				Alert.alert('There was an issue signing in')
+			}
+		}
+	}
 
 	return (
 		<View style={{ marginTop: 20 }}>
