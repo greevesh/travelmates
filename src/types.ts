8d4cd6b1669c28df4d52f5ccfee8276013d330dc
greevesh <hgreeves@protonmail.com>
2024-08-13 12:32:04 +0800

    configured screen

--- src/types.ts
+++ src/types.ts
@@ -4,8 +4,10 @@ interface RootStackParamList {
     'Sign Up': unknown
     'Sign In': unknown
     'Forgot Password': unknown
+    'Setup': unknown
 }
   
 export type SignUpScreenNavProp = NavigationProp<RootStackParamList, 'Sign Up'>
 export type SignInScreenNavProp = NavigationProp<RootStackParamList, 'Sign In'>
 export type ForgotPasswordScreenNavProp = NavigationProp<RootStackParamList, 'Forgot Password'>
+export type SetupScreenNavProp = NavigationProp<RootStackParamList, 'Setup'>
