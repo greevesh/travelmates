e4364c4217e1a1246e61e2388c34c398d0388e6a
greevesh <hgreeves@protonmail.com>
2024-08-18 11:32:14 +0800

    updated endpoint ip addresses

--- src/consts/api.ts
+++ src/consts/api.ts
@@ -1,2 +1,2 @@
-export const signUpEndpoint = 'http://192.168.110.187:3500/signup'
-export const signInEndpoint = 'http://192.168.110.187:3500/signin'
+export const signUpEndpoint = 'http://192.168.108.187:3500/signup'
+export const signInEndpoint = 'http://192.168.108.187:3500/signin'
