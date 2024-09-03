c6e18383a76619309876b1813ceb13e49fd2b2ce
greevesh <hgreeves@protonmail.com>
2024-09-03 11:03:52 +0800

    adjusted with of base button

--- src/components/base/Button.tsx
+++ src/components/base/Button.tsx
@@ -6,6 +6,7 @@ interface IBaseButtonProps {
 	onPress: (event: GestureResponderEvent) => void
     text: string
 	bgColor: string
+	w?: number
 	icon?: {
 		source: string
 		size: number
@@ -14,9 +15,9 @@ interface IBaseButtonProps {
 	children?: ReactNode
 }
 
-export default function BaseButton({ onPress, children, bgColor, mb, icon, text }: IBaseButtonProps) {
+export default function BaseButton({ onPress, children, bgColor, w, mb, icon, text }: IBaseButtonProps) {
 	return (
-		<Pressable onPress={onPress} style={[styles.button, { backgroundColor: bgColor, marginBottom: mb }]}>
+		<Pressable onPress={onPress} style={[styles.button, { backgroundColor: bgColor, width: w, marginBottom: mb }]}>
 			{icon && <Icon size={icon.size} source={icon.source} color='#fff' /> }
 			<Text style={styles.text}>{text}</Text>
 			{children && children}
@@ -33,7 +34,6 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 		columnGap: 10,
-		width: '100%',
 		marginTop: 20,
 	},
 	text: {
