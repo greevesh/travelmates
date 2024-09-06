51a2f32ab9f7d5b13cfff22af4f0abd71f307f0b
greevesh <hgreeves@protonmail.com>
2024-09-06 11:57:08 +0800

    can now delete uploaded profile pic

--- src/components/edit/buttons/DeletePhotoButton.tsx
+++ src/components/edit/buttons/DeletePhotoButton.tsx
@@ -1,13 +1,28 @@
-import { StyleSheet, Text } from 'react-native'
+import { Alert, StyleSheet, Text } from 'react-native'
 import { Button } from 'react-native-paper'
 
+import { useProfilePhotoStore } from '../../../stores/useProfilePhotoStore'
+
 export default function DeletePhotoButton() {
+	const setUploaded = useProfilePhotoStore((state) => state.setUploaded)
+
+	const handleDelete = () => {
+		setUploaded(false)
+	}
+
 	const handleClick = () => {
-		console.log('modal opened')
+		Alert.alert('Delete the photo?', '', [
+			{
+				text: 'Cancel',
+			},
+			{ text: 'DELETE', onPress: () => handleDelete() },
+		])
 	}
 
 	return (
-		<Button onPress={handleClick} style={styles.btn} icon="delete" labelStyle={{ color: '#ff3c33' }}><Text style={{ color: '#424242' }}>Delete</Text></Button>
+		<>
+			<Button onPress={handleClick} style={styles.btn} icon="delete" labelStyle={{ color: '#ff3c33' }}><Text style={{ color: '#424242' }}>Delete</Text></Button>
+		</>
 	)
 }
 
