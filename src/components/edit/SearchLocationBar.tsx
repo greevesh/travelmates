5349081c6b2da7c5a2c8f73837b22d2f51960242
greevesh <hgreeves@protonmail.com>
2024-09-23 15:42:23 +0800

    created and implemented separate component for search bar

---
+++ src/components/edit/SearchLocationBar.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react'
+import { Searchbar } from 'react-native-paper'
+// @ts-expect-error removes red squiggly for correct import of @env
+import { GOOGLE_PLACES_API_KEY } from '@env'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+interface SearchLocationBarProps {
+	setLocation: (location: string | undefined) => void
+}
+
+export default function SearchLocationBar({ setLocation }: SearchLocationBarProps) {
+	const [query, setQuery] = useState('')
+	const [places, setPlaces] = useState<Array<{ place_id: string; description: string }>>([])
+	const [error, setError] = useState<string | null>(null)
+
+	const fetchPlaces = async (input: string) => {
+		const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=${GOOGLE_PLACES_API_KEY}&language=en&types=(cities)`
+		try {
+			const response = await fetch(url)
+			const data = await response.json()
+			setPlaces(data.predictions)
+			setError(null)
+		} catch (error) {
+			setError('Failed to fetch places. Please try again.')
+		}
+	}
+
+	const handleLocationChange = (place: string) => {
+		setLocation(place)
+		setQuery(place)
+		setPlaces([])
+	}
+
+	return (
+		<>
+			<Searchbar
+				inputStyle={{ marginTop: -5 }}
+				mode='bar'
+				style={styles.searchbar}
+				value={query}
+				onChangeText={(text) => {
+					setQuery(text)
+					fetchPlaces(text)
+				}}
+				placeholder="Search location"
+				onClearIconPress={() => setLocation(undefined)}
+			/>
+			{error && <Text style={styles.errorText}>{error}</Text>}
+			<View style={styles.resultsContainer}>
+				{places.map((place) => (
+					<TouchableOpacity onPress={() => handleLocationChange(place.description)} key={place.place_id} style={styles.resultItem}>
+						<Text>{place.description}</Text>
+					</TouchableOpacity>
+				))}
+			</View>
+		</>
+	)
+}
+
+const styles = StyleSheet.create({
+	searchbar: {
+		height: 45,
+		width: 300,
+		borderRadius: 50,
+		marginRight: 7,
+		backgroundColor: '#f0f0f0'
+	},
+	resultsContainer: {
+		position: 'absolute',
+		top: 50,
+		width: 280,
+		marginTop: 10,
+		backgroundColor: '#fff',
+		borderColor: '#ccc',
+		zIndex: 1000
+	},
+	resultItem: {
+		padding: 10,
+		borderColor: '#ccc',
+		borderBottomWidth: 1
+	},
+	errorText: {
+		marginTop: 10,
+	},
+})
--- src/components/setup/SecondStep.tsx
+++ src/components/setup/SecondStep.tsx
@@ -1,37 +1,16 @@
 import { useState } from 'react'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import { Card, Icon, Searchbar, TextInput } from 'react-native-paper'
-// @ts-expect-error removes red squiggly for correct import of @env
-import { GOOGLE_PLACES_API_KEY } from '@env'
+import { Card, Icon, TextInput } from 'react-native-paper'
 import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker'
 
+import SearchLocationBar from '../edit/SearchLocationBar'
+
 export default function SecondStep() {
-	const [query, setQuery] = useState('')
-	const [places, setPlaces] = useState<Array<{ place_id: string; description: string }>>([])
-	const [error, setError] = useState<string | null>(null)
 	const [location, setLocation] = useState<string | undefined>(undefined)
 	const [startDate, setStartDate] = useState<Date | undefined>(undefined)
 	const [endDate, setEndDate] = useState<Date | undefined>(undefined)
 	const [showStartDatePicker, setShowStartDatePicker] = useState(false)
 	const [showEndDatePicker, setShowEndDatePicker] = useState(false)
-
-	const fetchPlaces = async (input: string) => {
-		const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=${GOOGLE_PLACES_API_KEY}&language=en&types=(cities)`
-		try {
-			const response = await fetch(url)
-			const data = await response.json()
-			setPlaces(data.predictions)
-			setError(null)
-		} catch (error) {
-			setError('Failed to fetch places. Please try again.')
-		}
-	}
-
-	const onLocationChange = (place: string) => {
-		setLocation(place)
-		setQuery(place)
-		setPlaces([])
-	}
 
 	const onStartDateChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
 		if (event.type === 'dismissed') {
@@ -56,26 +35,7 @@ export default function SecondStep() {
 	return (
 		<>
 			<Card.Actions style={styles.container}>
-				<Searchbar
-					inputStyle={{ marginTop: -5 }}
-					mode='bar'
-					style={styles.searchbar}
-					value={query}
-					onChangeText={(text) => {
-						setQuery(text)
-						fetchPlaces(text)
-					}}
-					placeholder="Search location"
-					onClearIconPress={() => setLocation(undefined)}
-				/>
-				{error && <Text style={styles.errorText}>{error}</Text>}
-				<View style={styles.resultsContainer}>
-					{places.map((place) => (
-						<TouchableOpacity onPress={() => onLocationChange(place.description)} key={place.place_id} style={styles.resultItem}>
-							<Text>{place.description}</Text>
-						</TouchableOpacity>
-					))}
-				</View>
+				<SearchLocationBar setLocation={setLocation} />
 				<View style={styles.datePickerContainer}>
 					<TouchableOpacity onPress={() => setShowStartDatePicker(true)}>
 						<TextInput
@@ -130,30 +90,6 @@ const styles = StyleSheet.create({
 		display: 'flex',
 		flexDirection: 'column',
 		position: 'relative'
-	},
-	searchbar: {
-		height: 45,
-		width: 300,
-		borderRadius: 50,
-		marginRight: 7,
-		backgroundColor: '#f0f0f0'
-	},
-	resultsContainer: {
-		position: 'absolute',
-		top: 50,
-		width: 280,
-		marginTop: 10,
-		backgroundColor: '#fff',
-		borderColor: '#ccc',
-		zIndex: 1000
-	},
-	resultItem: {
-		padding: 10,
-		borderColor: '#ccc',
-		borderBottomWidth: 1
-	},
-	errorText: {
-		marginTop: 10,
 	},
 	datePickerContainer: {
 		display: 'flex',
