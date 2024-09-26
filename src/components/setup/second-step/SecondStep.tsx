c633968db253801184e4c24552d89bebbac36ce9
greevesh <hgreeves@protonmail.com>
2024-09-26 14:31:11 +0800

    moved second step component

---
+++ src/components/setup/second-step/SecondStep.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import { StyleSheet, View } from 'react-native'
+import { Card } from 'react-native-paper'
+
+import SearchLocationBar from '../../edit/SearchLocationBar'
+import EndDatePicker from '../../edit/EndDatePicker'
+import StartDatePicker from '../../edit/StartDatePicker'
+import Output from './Output'
+
+export default function SecondStep() {
+	const [location, setLocation] = useState<string | undefined>(undefined)
+	const [startDate, setStartDate] = useState<Date | undefined>(undefined)
+	const [endDate, setEndDate] = useState<Date | undefined>(undefined)
+
+	return (
+		<>
+			<Card.Actions style={styles.container}>
+				<SearchLocationBar setLocation={setLocation} />
+				<View style={styles.datePickerContainer}>
+					<StartDatePicker startDate={startDate} setStartDate={setStartDate} />
+					<EndDatePicker endDate={endDate} setEndDate={setEndDate} />
+				</View>
+				<Output location={location} startDate={startDate} endDate={endDate} />
+			</Card.Actions>
+		</>
+	)
+}
+
+const styles = StyleSheet.create({
+	container: {
+		display: 'flex',
+		flexDirection: 'column',
+		position: 'relative'
+	},
+	datePickerContainer: {
+		display: 'flex',
+		flexDirection: 'row',
+		marginTop: 20,
+	}
+})
