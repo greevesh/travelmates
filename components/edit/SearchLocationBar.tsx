83f9c0e5aae7f47f99f9eb09b9cbe651dab1cd84
greevesh <hgreeves@protonmail.com>
2025-01-08 06:37:46 +0800

    updated dev api key

--- components/edit/SearchLocationBar.tsx
+++ components/edit/SearchLocationBar.tsx
@@ -15,8 +15,7 @@ export default function SearchLocationBar() {
 	const setLocation = useTripStore((state) => state.setLocation)
 
 	const fetchPlaces = async (input: string) => {
-		console.log('key: ', process.env.GOOGLE_PLACES_API_KEY)
-		const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=${process.env.GOOGLE_PLACES_API_KEY}&language=en&types=(cities)`
+		const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${input}&key=${process.env.EXPO_PUBLIC_GOOGLE_PLACES_API_KEY}&language=en&types=(cities)`
 		try {
 			const response = await fetch(url)
 			const data = await response.json()
