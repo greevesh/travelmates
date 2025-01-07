afdff2d6deb73aaa89aa779f91822c3595ceb007
greevesh <hgreeves@protonmail.com>
2025-01-08 06:48:44 +0800

    updated api endpoints

--- consts/api.ts
+++ consts/api.ts
@@ -1,5 +1,7 @@
 export const signUpEndpoint = 'http://192.168.108.187:3500/signup'
 export const signInEndpoint = 'http://192.168.108.187:3500/signin'
 export const signOutEndpoint = 'http://192.168.108.187:3500/signout'
-export const usersEndpoint = 'http://192.168.0.100:3500/users?q='
-export const setupEndpoint = 'http://192.168.0.100:3500/user-setup'
+export const usersEndpoint = 'http://192.168.0.102:3500/users?q='
+export const setupEndpoint = 'http://192.168.0.102:3500/user-setup'
+
+export const s3ProfilePicsEndpoint = 'https://travelmates-profile-pics.s3.ap-southeast-1.amazonaws.com/'
