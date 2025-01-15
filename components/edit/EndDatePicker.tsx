2c54bca1cfac69924f1996f5103bcc76ab367e9b
greevesh <hgreeves@protonmail.com>
2025-01-16 07:04:24 +0800

    date picker modal now automatically closes upon date selection

--- components/edit/EndDatePicker.tsx
+++ components/edit/EndDatePicker.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker'
 
 import { useTripStore } from '../../stores/useTripStore'
 
 export default function EndDatePicker() {
-	const [showEndDatePicker, setShowEndDatePicker] = useState(false)
-
 	const { startDate, endDate, setEndDate } = useTripStore((state) => ({
 		startDate: state.startDate,
 		endDate: state.endDate,
@@ -14,7 +11,6 @@ export default function EndDatePicker() {
 
 	const handleEndDateChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
 		event.type === 'dismissed' ? setEndDate(endDate) : setEndDate(selectedDate)
-		setShowEndDatePicker(!showEndDatePicker)
 	}
 
 	return (
@@ -24,8 +20,8 @@ export default function EndDatePicker() {
 				mode="date"
 				display="default"
 				onChange={handleEndDateChange}
-				onTouchCancel={() => setShowEndDatePicker(!showEndDatePicker)}
 				minimumDate={startDate ?? new Date()}
+				key={endDate?.toISOString()}
 			/>
 		</>
 	)
--- components/edit/StartDatePicker.tsx
+++ components/edit/StartDatePicker.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
 import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker'
 
 import { useTripStore } from '../../stores/useTripStore'
 
 export default function StartDatePicker() {
-	const [showStartDatePicker, setShowStartDatePicker] = useState(false)
-
 	const { startDate, setStartDate, endDate } = useTripStore((state) => ({
 		startDate: state.startDate,
 		setStartDate: state.setStartDate,
@@ -14,7 +11,6 @@ export default function StartDatePicker() {
 
 	const handleStartDateChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
 		event.type === 'dismissed' ? setStartDate(startDate) : setStartDate(selectedDate)
-		setShowStartDatePicker(!showStartDatePicker)
 	}
 
 	return (
@@ -24,9 +20,9 @@ export default function StartDatePicker() {
 				mode="date"
 				display="default"
 				onChange={handleStartDateChange}
-				onTouchCancel={() => setShowStartDatePicker(!showStartDatePicker)}
 				minimumDate={new Date()}
 				maximumDate={endDate}
+				key={startDate?.toISOString()}
 			/>
 		</>
 	)
