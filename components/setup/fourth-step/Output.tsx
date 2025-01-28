a21618662a5d5d85cce38901100945ea563efb50
greevesh <hgreeves@protonmail.com>
2025-01-28 14:26:25 +0700

    fourth step output updated

--- components/setup/fourth-step/Output.tsx
+++ components/setup/fourth-step/Output.tsx
@@ -1,6 +1,6 @@
 import { User } from "@/stores/useUserStore"
-import { StyleSheet, View, Text } from "react-native"
-import { IconButton } from "react-native-paper"
+import { StyleSheet, View } from "react-native"
+import ListItem from "./ListItem"
 
 interface IOutputProps {
     selectedUsers: User[],
@@ -10,37 +10,19 @@ interface IOutputProps {
 export default function Output({ selectedUsers, handleRemoveUser }: IOutputProps) {
     return (
         <View style={styles.container}>
-            {selectedUsers.map((user) => (
-                <View key={user._id} style={styles.output}>
-                    <Text>{user.username}</Text>
-                    <IconButton style={styles.icon} size={18} icon="close" onPress={() => handleRemoveUser(user)} accessibilityLabel={`Remove ${user.username}`} />
-                </View>
-            ))}
+			<ListItem number="1" selectedUser={selectedUsers[0]} handleRemoveUser={() => handleRemoveUser(selectedUsers[0])} />
+			<ListItem number="2" selectedUser={selectedUsers[1]} handleRemoveUser={() => handleRemoveUser(selectedUsers[1])} />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        top: 60,
 		display: 'flex',
-		flexDirection: 'row',
-		flexWrap: 'wrap',
+		flexDirection: 'column',
+		rowGap: 10,
+		top: 90,
+		left: 20,
+		width: '100%',
 	},
-	output: {
-		display: 'flex',
-		flexDirection: 'row',
-		alignItems: 'center',
-		margin: 10,
-		paddingHorizontal: 10,
-		backgroundColor: '#f5f5f5',
-		borderWidth: 0.5,
-		height: 30,
-		borderRadius: 50,
-	},
-    icon: {
-		height: 20,
-		width: 20,
-		marginRight: 0
-	}
 })
