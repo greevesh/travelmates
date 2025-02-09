85d2b347520464c5a23019b00472dcd4c0f4f326
greevesh <hgreeves@protonmail.com>
2025-02-09 21:47:45 +0800

    created placeholder hub page

---
+++ components/hub/Hub.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Hub() {
+    return (
+        <View>
+            <Text>This is the hub page.</Text>
+        </View>
+    )
+}
