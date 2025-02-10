c264569c5e2c6b5bd7a5a970fe4456dd4e4dda99
greevesh <hgreeves@protonmail.com>
2025-02-10 16:42:27 +0800

    removed linear gradient lib

--- app/index.tsx
+++ app/index.tsx
@@ -1,33 +1,23 @@
 import { ScrollView, StyleSheet, View } from 'react-native'
 
-import SetupCard from '../components/setup/Card'
+// import SetupCard from '../components/setup/Card'
 
-import SignInCard from '../components/auth/sign-in/Card'
+import SignInCard from '@/components/auth/sign-in/Card'
 
-import { LinearGradient } from 'expo-linear-gradient'
-
-export default function Index() {
+export default function Page() {
 	return (
 		<>
-			<LinearGradient
-				colors={[ '#72C6EF', '#004E8F' ]}
-				style={styles.gradientBackground}
-			>
-				<ScrollView contentContainerStyle={styles.container}>
-					<View style={styles.content}>
-						{/* <SignInCard /> */}
-						<SetupCard />
-					</View>
-				</ScrollView>
-			</LinearGradient>
+			<ScrollView contentContainerStyle={styles.container}>
+				<View style={styles.content}>
+					<SignInCard />
+					{/* <SetupCard /> */}
+				</View>
+			</ScrollView>
 		</>
 	)
 }
 
 const styles = StyleSheet.create({
-	gradientBackground: {
-		flex: 1
-	},
 	container: {
 		justifyContent: 'center',
 		alignItems: 'center',
