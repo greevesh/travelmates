7ae2ef39713f46193071023ed815d5bafba7415d
greevesh <hgreeves@protonmail.com>
2025-02-24 14:07:28 +0800

    added and reintegrated gradient lib and background

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,14 @@
 import { Stack } from "expo-router"
 import Navbar from "@/components/Navbar"
+import { View } from "react-native"
 
 export default function RootLayout() {
   return (
-    <>
+    <View style={{ backgroundColor: '#fff', flex: 1, zIndex: 99 }}>
       <Navbar />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
-    </>
+    </View>
   )
 }
--- app/setup.tsx
+++ app/setup.tsx
@@ -1,11 +1,14 @@
 import { StyleSheet, View } from "react-native"
 import SetupCard from "@/components/setup/Card"
+import { LinearGradient } from 'expo-linear-gradient'
 
 export default function Page() {
     return (
-        <View style={styles.container}>
-            <SetupCard />
-        </View>
+        <LinearGradient colors={['#3b5998', '#8b9dc3']}>
+            <View style={styles.container}>
+                <SetupCard />
+            </View>
+        </LinearGradient>
     )
 }
 
