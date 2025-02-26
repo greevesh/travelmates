d7490b4fe2086133c2ad5574232ab1ec55703669
greevesh <hgreeves@protonmail.com>
2025-02-26 14:59:14 +0800

    added gradient bg to forgot pass page and updated brand logo

--- app/forgot-password.tsx
+++ app/forgot-password.tsx
@@ -1,11 +1,14 @@
 import { View, StyleSheet } from "react-native"
 import ForgotPasswordCard from "@/components/auth/forgot-password/Card"
+import { LinearGradient } from "expo-linear-gradient"
 
 export default function Page() {
     return (
-        <View style={styles.container}>
-            <ForgotPasswordCard />
-        </View>
+        <LinearGradient colors={['#3b5998', '#8b9dc3']}>
+            <View style={styles.container}>
+                <ForgotPasswordCard />
+            </View>
+        </LinearGradient>
     )
 }
 
