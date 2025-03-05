04b55e29304899bab444d4eabc30c8223e3dff0d
greevesh <hgreeves@protonmail.com>
2025-03-05 14:54:03 +0800

    relevant default time data has been added

--- components/hub/Table.tsx
+++ components/hub/Table.tsx
@@ -1,8 +1,19 @@
 import fetchCurrentUserTrip from "@/utils/fetchCurrentUserTrip"
-import { useEffect } from "react"
-import { View, Text } from "react-native"
+import { useEffect, useState } from "react"
+import { DataTable } from 'react-native-paper'
+import { ScrollView, View, Text } from 'react-native'
+import React from "react"
 
 export default function Table() {
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    const [displayMonth, setDisplayMonth] = useState(monthNames[new Date().getMonth()])
+    const [month, setMonth] = useState(new Date().getMonth())
+    const [displayYear, setDisplayYear] = useState(new Date().getFullYear())
+    const [monthDaysLength, setMonthDaysLength] = useState(new Date(displayYear, month + 1, 0).getDate())
+    const [displayDays, setDisplayDays] = useState<number[] | undefined>(undefined)
+
+    const todaysDate = new Date().getDate()
+    
     const parseAndLogCurrentUserTrip = async () => {
         try {
             const currentUserTrip = await fetchCurrentUserTrip()
@@ -11,26 +22,43 @@ export default function Table() {
             const endDate = new Date(currentUserTrip.endDate)
             const startDay = startDate.getDate()
             const endDay = endDate.getDate()
-            console.log('location: ', location)
-            console.log('start date: ', startDate)
-            console.log('end date: ', endDate)
-            console.log('start day: ', startDay)
-            console.log('end day: ', endDay)
+            console.log('trip: ', { location, startDate, endDate, startDay, endDay })
         }
         catch (err) {
             console.error('Error storing current trip data: ', err)
         }
     }
 
+    const loadDisplayDays = () => {
+        let days = []
+        for (let i = todaysDate; i < monthDaysLength + 1; i++) {
+            days.push(i)
+        }
+        setDisplayDays(days)
+    }
+
     useEffect(() => {
+        loadDisplayDays()
         parseAndLogCurrentUserTrip()
     }, [])
 
     // logCurrentUser()
 
     return (
-        <View>
-            <Text>This is where the grid should be.</Text>
-        </View>
+        <>
+            <View style={{ height: 100, backgroundColor: '#fff' }}>
+                <ScrollView horizontal={true}>
+                    <DataTable style={{ width: 1100 }}>
+                        <DataTable.Header>
+                            <DataTable.Title style={{ flex: 3 }}>User</DataTable.Title>
+                            {displayDays && displayDays.map((day) => (
+                                <DataTable.Title key={day}>{day}</DataTable.Title>
+                            ))}
+                        </DataTable.Header>
+                    </DataTable>
+                </ScrollView>
+            </View>
+            <Text style={{ margin: 14 }}>{displayMonth} {displayYear}</Text>
+        </>
     )
 }
