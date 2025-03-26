3170afa22ad92638016d6774d149f11f769f5c5e
greevesh <hgreeves@protonmail.com>
2025-03-26 17:56:04 +0800

    added base styling to trip page

--- app/(tabs)/trips.tsx
+++ app/(tabs)/trips.tsx
@@ -1,10 +1,39 @@
-import { View, Text, StyleSheet } from "react-native"
+import BaseButton from "@/components/base/Button"
+import Title from "@/components/base/Title"
+import EndDatePicker from "@/components/edit/EndDatePicker"
+import SearchLocationBar from "@/components/edit/SearchLocationBar"
+import StartDatePicker from "@/components/edit/StartDatePicker"
+import { useTripStore } from "@/stores/useTripStore"
+import { LinearGradient } from "expo-linear-gradient"
+import { View, StyleSheet } from "react-native"
 
 export default function Trips() {
+    const { locationQuery: query, startDate, endDate } = useTripStore((state) => ({
+        locationQuery: state.locationQuery,
+        startDate: state.startDate,
+        endDate: state.endDate
+    }))
+
+    const btnDisabled = !query || !startDate || !endDate
+
     return (
-        <View style={styles.container}>
-            <Text>Trips</Text>
-        </View>
+        <LinearGradient colors={['#3b5998', '#8b9dc3']}>
+            <View style={styles.container}>
+                <View style={styles.card}>
+                    <View style={{ alignItems: 'center', width: '100%' }}>
+                        <Title style={styles.title}>Trips</Title>
+                        <SearchLocationBar />
+                        <View style={styles.dateContainer}>
+                            <StartDatePicker />
+                            <EndDatePicker />
+                        </View>
+                        <View style={{ width: '90%', marginLeft: 20 }}>
+                            <BaseButton disabled={btnDisabled} onPress={() => console.log('pressed')} icon={{ source: 'plus', size: 20 }} text="Add Trip" bgColor="#4285F4" mb={20} w={110} />
+                        </View>
+                    </View>
+                </View>
+            </View>
+        </LinearGradient>
     )
 }
 
@@ -13,5 +42,26 @@ const styles = StyleSheet.create({
         height: '100%',
         justifyContent: 'center', 
         alignItems: 'center'
+    },
+    card: {
+        width: '90%',
+		borderWidth: 1,
+        borderRadius: 20,
+		borderColor: '#d3d3d3',
+		backgroundColor: '#fff',
+		height: 350,
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 24,
+		textAlign: 'center',
+        margin: 20
+    },
+    dateContainer: {
+        flexDirection: 'row', 
+        justifyContent: 'flex-start', 
+        marginTop: 20,
+        marginLeft: 0,
+        width: '90%',
     }
 })
