c45e7280c46294053f9e1ce43382904e0dbcb655
greevesh <hgreeves@protonmail.com>
2025-04-22 12:41:42 +0800

    included new lib in date picker components

--- components/edit/EndDatePicker.tsx
+++ components/edit/EndDatePicker.tsx
@@ -1,33 +1,73 @@
-import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker'
+import { View, StyleSheet, TextInput } from 'react-native'
+import { Calendar, DateData } from 'react-native-calendars'
+import { useTripStore } from '@/stores/useTripStore'
+import { useEffect, useState } from 'react'
+import { formatDate, getThreeYearsFromToday, isoFormatDate } from '@/utils/dates'
+import useDisableDates from '@/hooks/useDisableDates'
 
-import { useTripStore } from '../../stores/useTripStore'
-import React from 'react'
+export default function EndDatePicker() {
+	const [visible, setVisible] = useState(false)
+	const disabledDates = useDisableDates()
 
-interface IEndDatePickerProps {
-	maxDate: Date | undefined
-}
-
-export default function EndDatePicker({ maxDate }: IEndDatePickerProps) {
 	const { startDate, endDate, setEndDate } = useTripStore((state) => ({
 		startDate: state.startDate,
 		endDate: state.endDate,
 		setEndDate: state.setEndDate,
 	}))
 
-	const handleEndDateChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
-		event.type === 'dismissed' ? setEndDate(endDate) : setEndDate(selectedDate)
+	const minDate = startDate ? isoFormatDate(startDate) : isoFormatDate(new Date())
+	const endDatePlaceholder = endDate ? formatDate(endDate) : formatDate(startDate) ? formatDate(startDate) : formatDate(new Date())
+
+	const handleDateSelect = (date: DateData) => {
+		setEndDate(new Date(date.dateString))
+		setVisible(false)
 	}
 
+	useEffect(() => {
+		console.log('end date: ', endDate)
+	}, [endDate])
+
 	return (
-		<>
-			<DateTimePicker
-				value={endDate || new Date()}
-				mode="date"
-				display="default"
-				onChange={handleEndDateChange}
-				minimumDate={startDate || new Date()}
-				maximumDate={maxDate}
+		<View>
+			<TextInput 
+				onPress={() => setVisible(true)} 
+				placeholder={endDatePlaceholder} 
+				placeholderTextColor='black'
+				style={styles.input}
+				readOnly 
 			/>
-		</>
+			{visible &&
+				<View style={styles.container}>
+					<Calendar
+						onDayPress={handleDateSelect}
+						markedDates={{
+							...disabledDates
+						}}
+						minDate={minDate}
+						maxDate={isoFormatDate(getThreeYearsFromToday())}
+					/>
+				</View>
+			}
+		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	input: {
+		height: 35,
+		width: 120,
+		margin: 12,
+		padding: 10,
+		borderRadius: 5,
+		backgroundColor: '#f0f0f0',
+	},
+	container: {
+		position: 'absolute',
+		right: 0,
+		zIndex: 50,
+		width: 250,
+		backgroundColor: '#fff',
+		borderRadius: 10,
+		padding: 10,
+	}
+})
--- components/edit/StartDatePicker.tsx
+++ components/edit/StartDatePicker.tsx
@@ -1,32 +1,70 @@
-import DateTimePicker, { DateTimePickerEvent } from '@react-native-community/datetimepicker'
-
-import { useTripStore } from '../../stores/useTripStore'
-import React from 'react'
+import { View, StyleSheet, TextInput } from 'react-native'
+import { Calendar, DateData } from 'react-native-calendars'
+import { useTripStore } from '@/stores/useTripStore'
+import { useEffect, useState } from 'react'
+import { formatDate, getThreeYearsFromToday, isoFormatDate } from '@/utils/dates'
+import useDisableDates from '@/hooks/useDisableDates'
 
 export default function StartDatePicker() {
+	const [visible, setVisible] = useState(false)
+	const disabledDates = useDisableDates()
+
 	const { startDate, setStartDate, endDate } = useTripStore((state) => ({
 		startDate: state.startDate,
 		setStartDate: state.setStartDate,
 		endDate: state.endDate,
 	}))
 
-	const handleStartDateChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
-		event.type === 'dismissed' ? setStartDate(startDate) : setStartDate(selectedDate)
+	const handleDateSelect = (date: DateData) => {
+		setStartDate(new Date(date.dateString))
+		setVisible(false)
 	}
 
-	const threeYearsFromToday = new Date()
-	threeYearsFromToday.setFullYear(threeYearsFromToday.getFullYear() + 3)
+	useEffect(() => {
+		console.log('start date: ', startDate)
+	}, [startDate])
 
 	return (
-		<>
-			<DateTimePicker
-				value={startDate || new Date()}
-				mode="date"
-				display="default"
-				onChange={handleStartDateChange}
-				minimumDate={new Date()}
-				maximumDate={endDate || threeYearsFromToday}
+		<View>
+			<TextInput 
+				onPress={() => setVisible(true)} 
+				placeholder={startDate ? formatDate(startDate) : formatDate(new Date())} 
+				placeholderTextColor='black'
+				style={styles.input}
+				readOnly 
 			/>
-		</>
+			{visible &&
+				<View style={styles.container}>
+					<Calendar
+						onDayPress={handleDateSelect}
+						markedDates={{
+							...disabledDates
+						}}
+						minDate={isoFormatDate(new Date())}
+						maxDate={isoFormatDate(endDate) || isoFormatDate(getThreeYearsFromToday())}
+					/>
+				</View>
+			}
+		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	input: {
+		height: 35,
+		width: 120,
+		marginLeft: 10,
+		marginTop: 12,
+		padding: 10,
+		borderRadius: 5,
+		backgroundColor: '#f0f0f0',
+	},
+	container: {
+		position: 'absolute',
+		zIndex: 50,
+		width: 250,
+		backgroundColor: '#fff',
+		borderRadius: 10,
+		padding: 10,
+	}
+})
