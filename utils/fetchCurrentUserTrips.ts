291a19f51c5750d50ffa13f75a5d3d4f2d8ef3e5
greevesh <hgreeves@protonmail.com>
2026-06-02 15:31:35 +0800

    fetch current user trips added

---
+++ utils/fetchCurrentUserTrips.ts
@@ -0,0 +1,16 @@
+import { currentUserTripsEndpoint } from "@/consts/api"
+import axios from 'axios'
+import { withAuthRetry } from "./auth"
+import { handleError } from "./errorHandler"
+
+export default async function fetchCurrentUserTrips() {
+    try {
+        const res = await withAuthRetry((headers) => axios.get(currentUserTripsEndpoint, { headers }))
+        if (__DEV__) console.log('fetched trips: ', res.data)
+        return res.data
+    }
+    catch(err) {
+        handleError(err, 'Failed to fetch the current trip')
+        throw err
+    }
+}
