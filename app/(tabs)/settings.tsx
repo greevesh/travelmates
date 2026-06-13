76a3638d83fca7af58c1c6e7112ab47c9ac5e67d
greevesh <hgreeves@protonmail.com>
2026-06-13 19:58:30 +0800

    added user info to settings view

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Animated } from 'react-native'
+import { StyleSheet, Animated, Text, View } from 'react-native'
 import { useEffect, useRef } from 'react'
 import SignOutButton from '@/components/auth/SignOutButton'
+import UserInfo from '@/components/settings/UserInfo'
 
 export default function Tab() {
   const slideAnim = useRef(new Animated.Value(1000)).current
@@ -15,6 +16,8 @@ export default function Tab() {
 
   return (
       <Animated.View style={[styles.container, { transform: [{ translateX: slideAnim }] }]}>
+        <UserInfo />
+        <View style={styles.spacer} />
         <SignOutButton />
       </Animated.View>
   )
@@ -23,10 +26,19 @@ export default function Tab() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'flex-end',
     backgroundColor: '#0047AB',
     height: '100%',
     width: '100%',
     zIndex: 99
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '700',
+    color: '#fff',
+    marginTop: 60,
+    marginLeft: 20,
+  },
+  spacer: {
+    flex: 1,
   },
 })
