adaa8b5421944b9196e218efe2509050279f273b
harrisongreeves@proton.me <harrisongreeves@gmail.com>
2026-09-13 19:21:26 +0100

    update api endpoints

--- consts/api.ts
+++ consts/api.ts
@@ -1,15 +1,15 @@
-const url = process.env.EXPO_PUBLIC_DEV_URL
+const url = __DEV__ ? process.env.EXPO_PUBLIC_DEV_URL : process.env.EXPO_PUBLIC_PROD_URL
 
-export const signUpEndpoint = `${url}signup`
-export const signInEndpoint = `${url}signin`
-export const signOutEndpoint = `${url}signout`
-export const refreshEndpoint = `${url}refresh`
+export const signUpEndpoint = `${url}signup/`
+export const signInEndpoint = `${url}signin/`
+export const signOutEndpoint = `${url}signout/`
+export const refreshEndpoint = `${url}refresh/`
 export const usersEndpoint = `${url}users?q=`
-export const friendsEndpoint = `${url}users/friends`
-export const currentUserEndpoint = `${url}currentUser`
-export const tripEndpoint = `${url}trip`
-export const tripsEndpoint = `${url}trips`
-export const currentUserTripsEndpoint = `${url}trips/currentUser`
+export const friendsEndpoint = `${url}users/friends/`
+export const currentUserEndpoint = `${url}currentUser/`
+export const tripEndpoint = `${url}trip/`
+export const tripsEndpoint = `${url}trips/`
+export const currentUserTripsEndpoint = `${url}trips/currentUser/`
 export const friendRequestsEndpoint = `${url}friend-requests/`
 
 export const s3ProfilePicsEndpoint = 'https://travelmates-profile-pics.s3.ap-southeast-1.amazonaws.com/'
